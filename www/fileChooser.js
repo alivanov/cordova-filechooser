3d245d50f8edb8e996ad0a0a13be7478ae19ac5d
윤영식 <ysyun@yuwin.co.kr>
2015-01-11 17:47:10 +0900

    Update fileChooser.js

--- www/fileChooser.js
+++ www/fileChooser.js
@@ -1,5 +1,16 @@
-module.exports = {
-    open: function (success, failure) {
-        cordova.exec(success, failure, "FileChooser", "open", []);
-    }
-};
+function FileChooser() {}
+
+FileChooser.prototype.open = function(success, failure) {
+  cordova.exec(success, failure, "FileChooser", "open", []);
+}
+
+var fileChooser = new FileChooser();
+module.exports = fileChooser;
+
+// Make plugin work under window.plugins
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.fileChooser) {
+    window.plugins.fileChooser = fileChooser;
+}
