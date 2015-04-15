7e4a97e817fa1f2276e06d8e8d0c1e661eaf488e
Alexey Ivanov <ivanov@intspirit.com>
2015-04-15 13:38:51 +0300

    callback.error on cancel

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -67,9 +67,7 @@ public class FileChooser extends CordovaPlugin {
 
             } else if (resultCode == Activity.RESULT_CANCELED) {
 
-                // TODO NO_RESULT or error callback?
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                callback.sendPluginResult(pluginResult);
+                callback.error("");
 
             } else {
 
